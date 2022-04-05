6e743c27e4ee414335149ae527aed22e73116474
Cole <cole.smith.code@gmail.com>
2022-04-05 11:12:21 -0600

    added comment

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// gets the id of 'name' to add a focus state to.
+// gets the id of 'name' to add a focus state to. Snippet from: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/focus#focus_on_a_text_field
 function nameFocus() {
     document.getElementById("name").focus();
 }
-
+console.log(nameFocus);
 //
 
 
