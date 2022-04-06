5ee4249ccd7f21b9cdcd5f770528d9506b6e274a
Cole <cole.smith.code@gmail.com>
2022-04-06 10:15:17 -0600

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
+
 // gets the id of 'name' to add a focus state to. Snippet from: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/focus#focus_on_a_text_field
 function nameFocus() {
     document.getElementById("name").focus();
 }
 console.log(nameFocus);
 
-//
-const displayOtherJob =  document.getElementById("")
-const otherJobFunc = 
+// variables to store the select, and text id fields in job roles. 
+const otherJobRole = document.getElementById("other-job-role");
+const jobRole = document.getElementById("title");
 
 
 
