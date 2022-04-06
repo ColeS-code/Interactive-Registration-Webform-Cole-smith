55ae5c27e2431c24b6c15df0a21d0a9cd51b042d
Cole <cole.smith.code@gmail.com>
2022-04-06 11:04:39 -0600

    added event listener for drop down menu

--- js/script.js
+++ js/script.js
@@ -5,13 +5,26 @@ focusMethod = function getFocus() {
     document.getElementById("name").focus();
 }
 
-//*Job Role*/ variables to store the select, and text id fields in job roles.
+//*Job Role*/ variables to store the select, and text id fields in job roles as well as hiding the other drop down menu as default.
 
 const otherJobRole = document.getElementById("other-job-role");
 
 const jobRole = document.getElementById("title");
 
 otherJobRole.hidden = true;
+
+// this listens for any change made to the drop down menu, once a change occurs the job role selection either hides or displays itself.
+jobRole.addEventListener("change", event => {
+    if (event.target.value === "other") {
+        otherJobRole.hidden = false;
+    } else if (event.target.value !== "other") {
+        otherJobRole.hidden = true;
+    }
+});
+
+//
+
+
 
 
 
