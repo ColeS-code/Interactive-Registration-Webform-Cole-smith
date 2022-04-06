8c1b59e58958f00b63c3eaa6dadc02c0d7cdd476
Cole <cole.smith.code@gmail.com>
2022-04-06 10:31:08 -0600

    refactored, finalized, variables for job roles

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 
-// gets the id of 'name' to add a focus state to. Snippet from: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/focus#focus_on_a_text_field
-function nameFocus() {
+//*Name Focus State*/ gets the id of 'name' to add a focus state to. Snippet from: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/focus#focus_on_a_text_field
+
+focusMethod = function getFocus() {
     document.getElementById("name").focus();
 }
-console.log(nameFocus);
 
-// variables to store the select, and text id fields in job roles. 
+//*Job Role*/ variables to store the select, and text id fields in job roles.
+
 const otherJobRole = document.getElementById("other-job-role");
+
 const jobRole = document.getElementById("title");
+
+otherJobRole.hidden = true;
+
 
 
 
